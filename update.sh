e63f0be9fe07a04bc4a2a65dc64dbb221aea3a8f
qiaowei.zhang <zhangqw424@gmail.com>
2018-12-22 10:55:01 +0100

    improve update.sh backup with timestamp

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
+timestamp=$( date +%Y%m%d_%H%M%S )
+
 echo -n "Update TMUX configuration..."
-cp -f $HOME/.tmux.conf $HOME/.tmux.conf_bak
+cp -f $HOME/.tmux.conf $HOME/.tmux.conf_${timestamp}
 cp -f tmux.conf $HOME/.tmux.conf
 echo "Done"
 
 echo -n "Update VIM configuration..."
-cp -f $HOME/.vimrc $HOME/.vimrc_bak
+cp -f $HOME/.vimrc $HOME/.vimrc_${timestamp}
 cp -f .vimrc $HOME/.vimrc
 echo "Done"
 
